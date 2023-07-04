ac911f01bd10699d1c664504a29269d5df5eee81
ryu38 <niceneetlife.ryu@gmail.com>
2023-07-04 11:01:24 +0900

    fix missing code

--- examples/community/stable_diffusion_controlnet_reference.py
+++ examples/community/stable_diffusion_controlnet_reference.py
@@ -239,6 +239,8 @@ class StableDiffusionControlNetReferencePipeline(StableDiffusionControlNetPipeli
         """
         assert reference_attn or reference_adain, "`reference_attn` or `reference_adain` must be True."
 
+        controlnet = self.controlnet._orig_mod if is_compiled_module(self.controlnet) else self.controlnet
+        
         # align format for control guidance
         if not isinstance(control_guidance_start, list) and isinstance(control_guidance_end, list):
             control_guidance_start = len(control_guidance_end) * [control_guidance_start]
@@ -276,8 +278,6 @@ class StableDiffusionControlNetReferencePipeline(StableDiffusionControlNetPipeli
         # of the Imagen paper: https://arxiv.org/pdf/2205.11487.pdf . `guidance_scale = 1`
         # corresponds to doing no classifier free guidance.
         do_classifier_free_guidance = guidance_scale > 1.0
-
-        controlnet = self.controlnet._orig_mod if is_compiled_module(self.controlnet) else self.controlnet
 
         if isinstance(controlnet, MultiControlNetModel) and isinstance(controlnet_conditioning_scale, float):
             controlnet_conditioning_scale = [controlnet_conditioning_scale] * len(controlnet.nets)
